75f9001016a4f7c5226a5c24aa096ca385c89dd7
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-03-01 12:40:27 +0530

    quiz1 revision

---
+++ quiz-1/calc.js
@@ -0,0 +1,5 @@
+console.log('calc.js running');
+
+export function add(num1, num2) {
+  return num1 + num2;
+}
---
+++ quiz-1/main.js
@@ -0,0 +1,5 @@
+import { add } from './calc.js';
+
+console.log('in main.js');
+
+console.log(add(20, 30));
