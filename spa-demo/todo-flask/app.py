13bc418dbf0c2e566d1656fefb693e4d0c578aa7
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2025-03-07 01:00:00 +0530

    todo spa and mpa

---
+++ spa-demo/todo-flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+todos = [
+    'Do yoga',
+    'Read books',
+    'Walk on land',
+    'Solve exercise 4.1',
+    'Eat lunch'
+
+]
+
+
+@app.route('/')
+def home():
+  return render_template('index.html', todos=todos)
+
+
+@app.route('/add-todo', methods=['POST'])
+def add_todo():
+  todo = request.form.get('todo')
+  todos.append(todo)
+  return redirect('/')
+
+
+app.run(debug=True)
